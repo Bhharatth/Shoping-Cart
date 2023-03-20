b0872c0235fcb7f78d70edd3f287797b8cf1c0a4
bharath <bharathprasannan96@gmail.com>
2023-03-20 07:44:06 +0530

    productadding feature added on admin panel

--- ecommerceapi/models/Product.js
+++ ecommerceapi/models/Product.js
@@ -7,8 +7,17 @@ const ProductSchema = new mongoose.Schema({
   categories: { type: Array },
   size: { type: Array },
   color: { type: Array },
-  price: { type: Number, required: true },
+  price: { type: Number, required: false },
   inStock: {type: Boolean, default: true},
+
+  // size: { type: Array },
+  // color: { type: Array },
+  // categories: { type: Array },
+  // desc: { type: String, required: true },
+  // img: { type: String, required: true },
+  // inStock: {type: Boolean, default: true},
+  // price: { type: Number, required: true },
+  // title: { type: String, unique: true, required: true },
 });
 
 module.exports = mongoose.model("Product", ProductSchema);
--- ecommerceapi/routes/products.js
+++ ecommerceapi/routes/products.js
@@ -5,13 +5,13 @@ const Product = require("../models/Product");
 const { verifyTokenAndAdmin } = require("./verifyToken");
 
 //CREATE
-router.post("/", async (req, res) => {
+router.post("/",verifyTokenAndAdmin, async (req, res) => {
   const newProduct = new Product(req.body);
   try {
     const savedProduct = await newProduct.save();
     res.status(200).json(savedProduct);
   } catch (error) {
-    res.status(500).json(err);
+    res.send(500).json(error);
   }
 });
 
